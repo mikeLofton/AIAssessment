057fd3fcb8f354a23600308b435f9a5894de6123
mikeLofton <michaellofton32@gmail.com>
2022-03-23 12:55:19 -0500

    sTUFF

--- raygame/StateMachineComponent.cpp
+++ raygame/StateMachineComponent.cpp
@@ -2,6 +2,7 @@
 #include "Actor.h"
 #include "SeekComponent.h"
 #include "WanderComponent.h"
+#include "PathfindComponent.h"
 #include "Transform2D.h"
 #include "Fruit.h"
 
@@ -11,6 +12,8 @@ void StateMachineComponent::start()
 
 	m_seekComponent = getOwner()->getComponent<SeekComponent>();
 	m_seekForce = m_seekComponent->getSteeringForce();
+
+	m_pathfindComp = getOwner()->getComponent<PathfindComponent>();
 
 	m_wanderComponent = getOwner()->getComponent<WanderComponent>();
 	m_wanderForce = m_wanderComponent->getSteeringForce();
@@ -25,9 +28,10 @@ void StateMachineComponent::update(float deltaTime)
 	switch (m_currentState)
 	{
 	case IDLE:
+		m_pathfindComp->setEnabled(false);
 		m_wanderComponent->setSteeringForce(0);
 
-		if (m_fruit->getActive() == true)
+		if (m_fruit->getActive())
 			setCurrentState(SEEKFRUIT);
 
 		break;
